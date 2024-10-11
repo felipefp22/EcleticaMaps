3731b09cb17bce68882f3998ecb0324f71b0d1e6
Felipe Ferreira de Paula <122496288+felipefp22@users.noreply.github.com>
2024-10-10 22:20:34 -0700

    here we goo!

--- delivery-map/src/App.js
+++ delivery-map/src/App.js
@@ -1,29 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,  useRef } from 'react';
 import './App.css';
 import L from 'leaflet'; // Import Leaflet
 
 
 function App() {
+
+  const mapRef = useRef(null); // Referência para o mapa
+
   useEffect(() => {
-    // Initialize the map in the 'mapa' div
-    const map = L.map('mapa').setView([-23.691047472947353, -46.714394001780896], 15); // Set the center and zoom level
+    // Inicializa o mapa
+    mapRef.current = L.map('mapa').setView([-23.691047472947353, -46.714394001780896], 15); // Define a centralização do mapa
 
-    // Add OpenStreetMap tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // Adiciona uma camada de tiles do OpenStreetMap
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    }).addTo(mapRef.current);
 
-    // Add ponto central, local do comercio to the map
-    L.marker([-23.691047472947353, -46.714394001780896]).addTo(map)
-      .bindPopup('Local inicial.')
-      .openPopup();
+    L.marker([-23.691047472947353, -46.714394001780896]).addTo(mapRef.current)
+    .bindPopup('Local inicial.')
+    .openPopup();
 
-    // Cleanup map on component unmount
     return () => {
-      map.remove();
+      mapRef.current.remove(); // Remove o mapa ao desmontar para evitar leaks de memória
     };
   }, []);
+
+  const centralizarMapa = () => {
+    // Centraliza o mapa na posição especificada
+    if (mapRef.current) {
+      mapRef.current.setView([-23.691047472947353, -46.714394001780896], 15);
+    }
+  };
+
 
   return (
     <div className="App">
@@ -32,6 +41,8 @@ function App() {
         <input type='text' placeholder='Endereço para centralizar Mapa' /> {/* Corrected here */}
         <button className='salvarLocalButton'>Salvar-Local</button>
         <button className='atualizarButton'>ATUALIZAR</button>
+        <button onClick={centralizarMapa}>Centralizar Mapa</button> {/* Botão para centralizar o mapa */}
+
       </div>
 
       <div className='mapa' id='mapa' style={{ height: '800px', width: '100%' }}></div> {/* Div for the map */}
