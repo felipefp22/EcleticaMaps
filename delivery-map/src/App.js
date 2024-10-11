2085887574eb67ed8f4eb87386db51f1f5742ba8
Felipe Ferreira de Paula <122496288+felipefp22@users.noreply.github.com>
2024-10-10 21:58:36 -0700

    issae

--- delivery-map/src/App.js
+++ delivery-map/src/App.js
@@ -1,18 +1,41 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import L from 'leaflet'; // Import Leaflet
+
 
 function App() {
+  useEffect(() => {
+    // Initialize the map in the 'mapa' div
+    const map = L.map('mapa').setView([-23.691047472947353, -46.714394001780896], 15); // Set the center and zoom level
+    
+    // Add OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Add marker to the map
+    L.marker([-23.691047472947353, -46.714394001780896]).addTo(map)
+      .bindPopup('Local inicial.')
+      .openPopup();
+      
+    // Cleanup map on component unmount
+    return () => {
+      map.remove();
+    };
+  }, []);
+
   return (
     <div className="App">
 
       <div className='barraSuperior'>
-        <imput type='text' placeholder='Digite o seu endereço'> </imput>
-        <button >Salvar Meu Local</button>
+      <input type='text' placeholder='Endereço para centralizar Mapa' /> {/* Corrected here */}
+      <button >Salvar-Local</button>
         <button >ATUALIZAR</button>
       </div>
 
-      <div className='mapa'>
-      </div>
+      <div className='mapa' id='mapa' style={{ height: '800px', width: '100%' }}></div> {/* Div for the map */}
+
 
       <div className='barraPedidos'>
       </div>
