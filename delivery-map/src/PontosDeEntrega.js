af83c27f2a63e623d2ab7642d411bdaf3907e5cf
Felipe Ferreira de Paula <122496288+felipefp22@users.noreply.github.com>
2024-10-18 23:38:04 -0700

    cores bem arrumado

--- delivery-map/src/PontosDeEntrega.js
+++ delivery-map/src/PontosDeEntrega.js
@@ -15,7 +15,11 @@ export const PontosDeEntrega = ({ map, lat, lng, label, minutes }) => {
         line-height: 40px;
         font-weight: 800;
         color: white;
-        border-radius: 50px;">
+        font-size: 21px;
+        border-radius: 50px;
+                -webkit-text-stroke: 1.5px black;  /* Border around the text */
+        // text-shadow: 1px 1px 0 black;    /* Shadow to create a border effect */
+        ">
         ${label}
       </div>`,
         iconSize: [40, 40],
@@ -26,98 +30,161 @@ export const PontosDeEntrega = ({ map, lat, lng, label, minutes }) => {
 
     L.marker([lat, lng], { icon: pizzaIcon })
         .addTo(map)
-        .bindPopup(`${label} minutes`)
+        .bindPopup(`${minutes}min`)
         .openPopup();
 };
 
 function selectMarkerColor(minutes) {
     if (minutes <= 5) {
-        return '#a19709';
+        return '#fffb0b';
     } else if (minutes > 5 && minutes <= 10) {
-        return '#0279e9';
+        return '#a1ff0b';
     } else if (minutes > 10 && minutes <= 15) {
-        return '#a19709';
+        return '#0c97e7';
     } else if (minutes > 15 && minutes <= 20) {
-        return '#0279e9';
+        return '#0c6be7';
+
     } else if (minutes > 20 && minutes <= 25) {
-        return '#a19709';
+        return '#0e1dfc';
     } else if (minutes > 25 && minutes <= 30) {
-        return '#a19709';
+        return '#5715f1';
     } else if (minutes > 30 && minutes <= 35) {
-        return '#a19709';
+        return '#b6118d';
     } else if (minutes > 35 && minutes <= 40) {
-        return '#0279e9';
+        return '#eb2778';
+
     } else if (minutes > 40 && minutes <= 45) {
-        return '#0279e9';
+        return '#eb8627';
     } else if (minutes > 45 && minutes <= 50) {
-        return '##a19709';
+        return '#e42e0e';
     } else if (minutes > 50 && minutes <= 55) {
-        return '#0279e9';
+        return '#e40e0e';
     } else if (minutes > 55 && minutes <= 60) {
-        return '#a19709';
+        return '#e40e0e; border: 5px solid #f36818';
+
+
     } else if (minutes > 60 && minutes <= 65) {
-        return '#a19709';
+        return '#e40e0e; border: 10px solid #f36818'
     } else if (minutes > 65 && minutes <= 70) {
-        return '#a19709';
+        return '#e40e0e; border: 15px solid #f36818'
+    } else if (minutes > 70) {
+        return '#e40e0e; border: 20px solid #f36818'
     }
 }
 
-export const fetchPontosDeEntrega = async () => {
+export const fetchPontosDeEntregaTeste = async () => {
     return [
         {
-            lat: -23.691106873992,
-            lng: -46.71366202199305,
+            lat: -23.689106873992,
+            lng: -46.710394001780896,
             label: '1',
             minutes: 1
         },
         {
-            lat: -23.694047472947353,
-            lng: -46.718394001780896,
+            lat: -23.695047472947353,
+            lng: -46.708394001780896,
             label: '2',
             minutes: 6
         },
         {
             lat: -23.689047472947353,
-            lng: -46.714394001780896,
+            lng: -46.704394001780896,
             label: '3',
-            minutes: 11
+            minutes: 1
         },
         {
             lat: -23.691047472947353,
-            lng: -46.710394001780896,
+            lng: -46.705394001780896,
             label: '4',
-            minutes: 15
-        }
-    ];
-}
-
-
-
-export const fetchPontosDeEntregaTeste = async () => {
-    return [
+            minutes: 16
+        },
         {
-            lat: -23.691106873992,
-            lng: -46.71366202199305,
-            label: '1',
-            minutes: 8
+            lat: -23.695106873992,
+            lng: -46.71266202199305,
+            label: '5',
+            minutes: 21
+        },
+        {
+            lat: -23.691047472947353,
+            lng: -46.708394001780896,
+            label: '6',
+            minutes: 26
+        },
+        {
+            lat: -23.687047472947353,
+            lng: -46.702394001780896,
+            label: '7',
+            minutes: 31
+        },
+        {
+            lat: -23.695047472947353,
+            lng: -46.710394001780896,
+            label: '8',
+            minutes: 36
+        },        {
+            lat: -23.696106873992,
+            lng: -46.71166202199305,
+            label: '9',
+            minutes: 41
         },
         {
             lat: -23.694047472947353,
-            lng: -46.718394001780896,
-            label: '2',
-            minutes: 11
+            lng: -46.712394001780896,
+            label: '10',
+            minutes: 46
         },
         {
-            lat: -23.689047472947353,
-            lng: -46.714394001780896,
-            label: '3',
-            minutes: 25
+            lat: -23.69395047472947353,
+            lng: -46.709394001780896,
+            label: '11',
+            minutes: 51
         },
         {
             lat: -23.691047472947353,
+            lng: -46.711394001780896,
+            label: '12',
+            minutes: 56
+        },        {
+            lat: -23.689106873992,
+            lng: -46.70866202199305,
+            label: '13',
+            minutes: 61
+        },
+        {
+            lat: -23.692047472947353,
             lng: -46.710394001780896,
-            label: '4',
-            minutes: 20
+            label: '14',
+            minutes: 66
         }
     ];
 }
+
+//export const fetchPontosDeEntrega = async () => {
+    //     return [
+    //         {
+    //             lat: -23.691106873992,
+    //             lng: -46.71366202199305,
+    //             label: '1',
+    //             minutes: 1
+    //         },
+    //         {
+    //             lat: -23.694047472947353,
+    //             lng: -46.718394001780896,
+    //             label: '2',
+    //             minutes: 6
+    //         },
+    //         {
+    //             lat: -23.689047472947353,
+    //             lng: -46.714394001780896,
+    //             label: '3',
+    //             minutes: 11
+    //         },
+    //         {
+    //             lat: -23.691047472947353,
+    //             lng: -46.710394001780896,
+    //             label: '4',
+    //             minutes: 15
+    //         }
+    //     ];
+    // }
+    
