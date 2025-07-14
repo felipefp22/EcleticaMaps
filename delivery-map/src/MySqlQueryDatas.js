e68e5ca2dd34771ad5764eb05bcd7a402269bede
Felipe Ferreira de Paula <felipefp2@gmail.com>
2025-07-14 12:15:39 -0300

    Agr separei Mapa para outro arquivo

--- delivery-map/src/MySqlQueryDatas.js
+++ delivery-map/src/MySqlQueryDatas.js
@@ -1,69 +1,69 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-// Configuração de conexão com o banco de dados
-const connection = mysql.createConnection({
-  host: 'localhost',        // O host onde o MySQL está rodando
-  user: 'root',             // Usuário do MySQL
-  password: 'minha_senha',  // Senha do MySQL
-  database: 'ecletica'      // Nome do banco de dados
-});
+// // Configuração de conexão com o banco de dados
+// const connection = mysql.createConnection({
+//   host: 'localhost',        // O host onde o MySQL está rodando
+//   user: 'root',             // Usuário do MySQL
+//   password: 'minha_senha',  // Senha do MySQL
+//   database: 'ecletica'      // Nome do banco de dados
+// });
 
-// Função para conectar ao banco de dados
-function connectToDatabase() {
-  return new Promise((resolve, reject) => {
-    connection.connect(err => {
-      if (err) {
-        reject('Erro ao conectar ao banco de dados: ' + err.message);
-      } else {
-        resolve('Conectado ao banco de dados MySQL.');
-      }
-    });
-  });
-}
+// // Função para conectar ao banco de dados
+// function connectToDatabase() {
+//   return new Promise((resolve, reject) => {
+//     connection.connect(err => {
+//       if (err) {
+//         reject('Erro ao conectar ao banco de dados: ' + err.message);
+//       } else {
+//         resolve('Conectado ao banco de dados MySQL.');
+//       }
+//     });
+//   });
+// }
 
-// Função para executar a query
-function queryDatabase(query) {
-  return new Promise((resolve, reject) => {
-    connection.query(query, (err, results) => {
-      if (err) {
-        reject('Erro ao executar a query: ' + err.message);
-      } else {
-        resolve(results);
-      }
-    });
-  });
-}
+// // Função para executar a query
+// function queryDatabase(query) {
+//   return new Promise((resolve, reject) => {
+//     connection.query(query, (err, results) => {
+//       if (err) {
+//         reject('Erro ao executar a query: ' + err.message);
+//       } else {
+//         resolve(results);
+//       }
+//     });
+//   });
+// }
 
-// Função para encerrar a conexão com o banco
-function closeConnection() {
-  return new Promise((resolve, reject) => {
-    connection.end(err => {
-      if (err) {
-        reject('Erro ao encerrar a conexão: ' + err.message);
-      } else {
-        resolve('Conexão encerrada.');
-      }
-    });
-  });
-}
+// // Função para encerrar a conexão com o banco
+// function closeConnection() {
+//   return new Promise((resolve, reject) => {
+//     connection.end(err => {
+//       if (err) {
+//         reject('Erro ao encerrar a conexão: ' + err.message);
+//       } else {
+//         resolve('Conexão encerrada.');
+//       }
+//     });
+//   });
+// }
 
-// Função principal para realizar consulta
-export async function fetchData() {
-  try {
-    // Conectar ao banco de dados
-    const connectionMessage = await connectToDatabase();
-    console.log(connectionMessage);
+// // Função principal para realizar consulta
+// export async function fetchData() {
+//   try {
+//     // Conectar ao banco de dados
+//     const connectionMessage = await connectToDatabase();
+//     console.log(connectionMessage);
 
-    // Realizar a consulta (por exemplo, buscar todos os registros da tabela "pedidos")
-    const query = 'SELECT * FROM pedidos';
-    const results = await queryDatabase(query);
-    console.log('Resultados da tabela pedidos:', results);
+//     // Realizar a consulta (por exemplo, buscar todos os registros da tabela "pedidos")
+//     const query = 'SELECT * FROM pedidos';
+//     const results = await queryDatabase(query);
+//     console.log('Resultados da tabela pedidos:', results);
 
-    // Fechar a conexão
-    const closeMessage = await closeConnection();
-    console.log(closeMessage);
-    return results;
-  } catch (error) {
-    console.error('Erro:', error);
-  }
-}
+//     // Fechar a conexão
+//     const closeMessage = await closeConnection();
+//     console.log(closeMessage);
+//     return results;
+//   } catch (error) {
+//     console.error('Erro:', error);
+//   }
+// }
--- delivery-map/src/letMap.js
+++ delivery-map/src/letMap.js
@@ -1,18 +1,18 @@
-import L from 'leaflet'; // Importa o Leaflet
+// import L from 'leaflet'; // Importa o Leaflet
 
-export function initMap() {
-  // Inicializa o mapa
-  const map = L.map('map').setView([-23.691047472947353, -46.714394001780896], 20); // Define a centralização do mapa
+// export function initMap() {
+//   // Inicializa o mapa
+//   const map = L.map('map').setView([-23.691047472947353, -46.714394001780896], 20); // Define a centralização do mapa
 
-  // Adiciona uma camada de tiles do OpenStreetMap
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  }).addTo(map);
+//   // Adiciona uma camada de tiles do OpenStreetMap
+//   L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+//   }).addTo(map);
 
-  // Adiciona um marcador em Brasília
-  const marker = L.marker([-23.691047472947353, -46.714394001780896]).addTo(map);
-  marker.bindPopup('<b>Brasília</b><br>Capital do Brasil.').openPopup();
+//   // Adiciona um marcador em Brasília
+//   const marker = L.marker([-23.691047472947353, -46.714394001780896]).addTo(map);
+//   marker.bindPopup('<b>Brasília</b><br>Capital do Brasil.').openPopup();
 
-  return map; // Retorna o mapa
-}
+//   return map; // Retorna o mapa
+// }
